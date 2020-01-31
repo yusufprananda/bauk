cb766aff2cf0cb8d1e85e92bc300c1cb67fb33df
Yusuf Prananda <nandaarankaru@gmail.com>
2020-01-31 15:02:52 +0700

    Fix navbar responsive look.

--- functions/navbar.php
+++ functions/navbar.php
@@ -1,37 +1,45 @@
-    <!-- Navbar -->
-    <nav class="cNavbar">
-        <div class="d-flex flex-xl-row flex-column">
-            <div class="p-2 my-auto">
-                <a href="">Home</a>
-            </div>
-            <div class="p-2 my-auto">
-                <a href="">Struktur Organisasi</a>
-            </div>
-            <div class="p-2 my-auto">
-                <a href="">Aset</a>
-            </div>
-            <div class="p-2 my-auto">
-                <a href="">Pengadaan Barang</a>
-            </div>
-            <div class="p-2 my-auto">
-                <a href="">Jadwal Gedung</a>
-            </div>
-            <div class="p-2 my-auto">
-                <a href="">Transportasi</a>
-            </div>
-            <div class="p-2 my-auto">
-                <a href="">Renovasi</a>
-            </div>
-            <div class="p-2 my-auto">
-                <a href="">Pijam Alat</a>
-            </div>
-            <div class="p-2 my-auto ml-auto">
-                <div class="input-group mt-1">
-                    <input type="text" class="form-control" placeholder="Search ....">
-                    <div class="input-group-append">
-                        <button class="btn btn-info">Search</button>
-                    </div>
+<nav class="cNavbar navbar navbar-expand-lg navbar-dark">
+    <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Beranda</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Tentang
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Struktur Organisasi</a>
+                    <a class="dropdown-item" href="#">Profil Staff</a>
                 </div>
-            </div>
-        </div>
-    </nav>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Aset</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pengadaan Barang</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Jadwal Gedung</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Transportasi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Renovasi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Peminjaman Alat</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search ..." aria-label="Search">
+            <button class="btn btn-info my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
