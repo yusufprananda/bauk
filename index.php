5084c92a08be0ad5d923800247e006a54dadfa40
Yusuf Prananda <nandaarankaru@gmail.com>
2020-01-31 11:14:58 +0700

    Add Header, Navbar, Bottom Widget, and Footer. Styling it too.

--- index.php
+++ index.php
@@ -16,19 +16,127 @@
     <!-- My Style -->
     <link rel="stylesheet" href="css/style.css">
 
+    <!-- Link Font -->
+    <!-- Noto Serif and Roboto Font -->
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i|Roboto:400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    <!-- Header -->
+    <header class="cHeader">
+        <div class="d-flex flex-row">
+            <div class="my-4">
+                <h1>Biro Administrasi Umum</h1>
+            </div>
+            <div class="my-4 ml-auto">
+                <div class="input-group mt-1">
+                    <input type="text" class="form-control" placeholder="Search ....">
+                    <div class="input-group-append">
+                        <button class="btn btn-info">Search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Navbar -->
+    <nav class="cNavbar">
+        <div class="d-flex flex-row">
+            <div class="p-2">
+                <a href="">Home</a>
+            </div>
+            <div class="p-2">
+                <a href="">Struktur Organisasi</a>
+            </div>
+            <div class="p-2">
+                <a href="">Aset</a>
+            </div>
+            <div class="p-2">
+                <a href="">Pengadaan Barang</a>
+            </div>
+            <div class="p-2">
+                <a href="">Jadwal Gedung</a>
+            </div>
+            <div class="p-2">
+                <a href="">Transportasi</a>
+            </div>
+            <div class="p-2">
+                <a href="">Renovasi</a>
+            </div>
+            <div class="p-2">
+                <a href="">Pijam Alat</a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Content -->
+
+    <!-- Widget Bottom -->
+    <section class="cWidbot">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 text-center p-4">
+                    <img class="imgLogo" src="img/logo.png" alt="Logo Universitas Ngudi Waluyo">
+                    <p>Universitas Ngudi Waluyo<br>Kabupaten Semarang, Jawa Tengah, Indonesia.</p>
+                </div>
+                <div class="col-md-3 p-4">
+                    <h4>Menu</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Struktur Organisasi</a>
+                        </li>
+                        <li>
+                            <a href="#">Aset</a>
+                        </li>
+                        <li>
+                            <a href="#">Pengadaan Barang</a>
+                        </li>
+                        <li>
+                            <a href="#">Jadwal Gedung</a>
+                        </li>
+                        <li>
+                            <a href="#">Transportasi</a>
+                        </li>
+                        <li>
+                            <a href="#">Renovasi</a>
+                        </li>
+                        <li>
+                            <a href="#">Pinjam Alat</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3 p-4">
+                    <h4>Kantor</h4>
+                    <p>Jl. Diponegoro no 186 Gedanganak - Ungaran Timur, Kab. Semarang Jawa Tengah</p>
+                    <p>Telp: (024)-6925408<br>Fax: (024)-6925408</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="cFooter">
+        <div class="d-flex flex-row-reverse">
+            <div class="p-3">
+                <p class="m-0">Copyright © <?= date("Y") ?> | Biro Administrasi Umum | Universitas Ngudi Waluyo | All Rights Reserved</p>
+            </div>
+        </div>
+    </footer>
 
     <!-- Link Script -->
+    <!-- JQuery 3.4.1 -->
+    <script src="js/jquery-3.4.1.min.js"></script>
     <!-- Bootstrap 4 -->
     <script src="js/bootstrap.min.js"></script>
     <!-- Font Awesome -->
     <script src="js/all.min.js"></script>
-    <!-- JQuery 3.4.1 -->
-    <script src="js/jquery-3.4.1.min.js"></script>
     <!-- My Script -->
-    
+    <script>
+
+    </script>
 </body>
 
 </html>
