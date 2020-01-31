ecc790780cb9f93a432f6fcb498615ca980bb8eb
Yusuf Prananda <nandaarankaru@gmail.com>
2020-01-31 09:20:18 +0700

    Init folder and finish setup all components.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Title -->
+    <title>Badan Administrasi Umum</title>
+
+    <!-- Link Style -->
+    <!-- Bootstrap 4 -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!-- My Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <!-- Link Script -->
+    <!-- Bootstrap 4 -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Font Awesome -->
+    <script src="js/all.min.js"></script>
+    <!-- JQuery 3.4.1 -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <!-- My Script -->
+    
+</body>
+
+</html>
