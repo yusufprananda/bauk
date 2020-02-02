51d19f0db54b486345d31efadd1bb15c8cebc3e2
Yusuf Prananda <nandaarankaru@gmail.com>
2020-02-02 23:42:41 +0700

    Change header font size and styling

--- functions/header.php
+++ functions/header.php
@@ -5,7 +5,7 @@
                 <img class="imgLogo" src="img/logo.png" alt="Logo Universitas Ngudi Waluyo">
             </div>
             <div class="my-auto ml-3">
-                <h1>Biro Administrasi Umum</h1>
+                <h2>Biro Administrasi Umum</h2>
                 <p>Universitas Ngudi Waluyo</p>
             </div>
         </div>
